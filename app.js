2a5944a0397a7c8ef9b1e49991993b840e484f72
Lizzie Li <yuanbodeng@Yuanbos-iMac.local>
2022-02-28 12:19:49 +1000

    Added salting and hashing password by bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const app = express();
 mongoose.connect("mongodb://localhost:27017/userDB");
 app.use(express.static("public"));
@@ -15,7 +16,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -31,31 +31,38 @@ app.get("/register", function(req,res){
   res.render("register");
 });
 
-app.post("/register", function(req,res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
+
+  app.post("/register", function(req,res){
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    });
   });
 });
+
 
 app.post("/login", function(req,res){
   const username = req.body.username;
   const password = req.body.password;
   User.findOne({email: username}, function(err, foundUser){
     if(foundUser){
-    if(foundUser.password === password ){
-      res.render("secrets");
-    }else{
-      res.send("Invalid password,please try again.");
+      bcrypt.compare(password, foundUser.password, function(err, result) {
+        if(result){
+          res.render("secrets");
+        }else{
+          res.send("Invalid password,please try again.");
 
-    }
+        }
+});
+
   }else{
     res.send("Invalid username, please try again.");
   }
